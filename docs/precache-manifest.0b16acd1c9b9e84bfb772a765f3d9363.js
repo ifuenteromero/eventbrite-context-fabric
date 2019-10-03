4b2389d5e0fc1168e4623a257cef86e81e39665f
Irene Fuente Romero <irene.fuente@ultimasoftware.com>
2019-10-03 17:43:34 +0200

    create docs folder

---
+++ docs/precache-manifest.0b16acd1c9b9e84bfb772a765f3d9363.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5f9cdd4be3768f71f861292c3d135b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b10469689dced6914ae",
+    "url": "/static/css/main.62cb1aec.chunk.css"
+  },
+  {
+    "revision": "037c7707441e03e7312d",
+    "url": "/static/js/2.6b46145a.chunk.js"
+  },
+  {
+    "revision": "2b10469689dced6914ae",
+    "url": "/static/js/main.ccc8c3d0.chunk.js"
+  },
+  {
+    "revision": "3f44e6f80b2e27cf1618",
+    "url": "/static/js/runtime-main.efa429fc.js"
+  }
+]);
